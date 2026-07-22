089cc2baf11c5b54a19b36c3dbd818658c31bb27
Volker Kuntner <git.dsea@tutanota.com>
2026-07-22 09:50:31 +0200

    extract graphics loading fns

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -2,13 +2,14 @@ use bevy::prelude::*;
 
 pub mod components;
 pub mod resources;
+mod loader;
 mod systems;
 
 pub struct GraphicsPlugin;
 impl Plugin for GraphicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, systems::load_tileset_assets)
-            .add_systems(Startup, systems::load_woozzle_assets)
+        app.add_systems(Startup, loader::load_tileset_assets)
+            .add_systems(Startup, loader::load_woozzle_assets)
             .add_systems(Update, systems::animate_sprites)
             .add_observer(systems::remove_tile_sprite)
             .add_observer(systems::insert_tile_sprite)
--- src/graphics/systems.rs
+++ src/graphics/systems.rs
@@ -5,8 +5,6 @@ use crate::map;
 use crate::woozzle;
 use bevy::prelude::*;
 
-const PATH_PREFIX: &str = "/assets/";
-
 pub fn insert_woozzle_sprite(
     _trigger: On<woozzle::events::VisibleUpdated>,
     visible_woozzles: Res<woozzle::resources::Visible>,
@@ -93,43 +91,4 @@ pub fn insert_tile_sprite(
         ));
     }
 }
-pub fn load_woozzle_assets(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    const PATH: &str = "images/woozzle.png";
-    const SIZE: u32 = 16;
-    const COLUMNS: u32 = 2;
-    const ROWS: u32 = 2;
 
-    let layout = TextureAtlasLayout::from_grid(UVec2::new(SIZE, SIZE), COLUMNS, ROWS, None, None);
-    commands.insert_resource(resources::WoozzleAsset {
-        image: asset_server.load::<Image>(PATH),
-        layout: texture_atlas_layouts.add(layout),
-    });
-    println!("Loading Woozzle asset, from: {}{}", PATH_PREFIX, PATH);
-}
-
-pub fn load_tileset_assets(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
-) {
-    const PATH: &str = "images/tileset32.png";
-    const SIZE: u32 = 32;
-    const ROWS: u32 = 2;
-
-    let layout = TextureAtlasLayout::from_grid(
-        UVec2::new(SIZE, SIZE),
-        map::components::TerrainType::n_of_types() as u32,
-        ROWS,
-        None,
-        None,
-    );
-    commands.insert_resource(resources::TilesetAsset {
-        image: asset_server.load::<Image>(PATH),
-        layout: texture_atlas_layouts.add(layout),
-    });
-    println!("Loading tileset, from: {}{}", PATH_PREFIX, PATH);
-}
